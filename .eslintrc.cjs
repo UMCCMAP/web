1b3b9eca75dbffad1b76b2a98905420bb081b87c
minjilee <budlalswl1129@gmail.com>
2023-07-05 15:20:07 +0900

    chore: react setting
    웹 리액트 초기세팅(vite+react+prettier+eslint)

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
