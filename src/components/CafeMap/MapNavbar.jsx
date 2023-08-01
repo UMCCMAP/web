9cdfa337ac11760fb624232894830f75d5ca2e4c
minjilee <budlalswl1129@gmail.com>
2023-08-01 21:26:20 +0900

    chore: change icon color

--- src/components/CafeMap/MapNavbar.jsx
+++ src/components/CafeMap/MapNavbar.jsx
@@ -1,61 +1,70 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Home } from '../../assets/images/home.svg';
+import { ReactComponent as Book } from '../../assets/images/book.svg';
+import { ReactComponent as Coffee } from '../../assets/images/coffee.svg';
+import { ReactComponent as Mountain } from '../../assets/images/mountain.svg';
+import { ReactComponent as Car } from '../../assets/images/car.svg';
+import { ReactComponent as Photo } from '../../assets/images/photo.svg';
+import { ReactComponent as Bread } from '../../assets/images/bread.svg';
+import { ReactComponent as Cake } from '../../assets/images/cake.svg';
+import { ReactComponent as Pasta } from '../../assets/images/pasta.svg';
 
-const navItems = [
-  {
-    id: 1,
-    name: 'home',
-    image: 'src/assets/images/home.svg',
-  },
-  {
-    id: 2,
-    name: 'book',
-    image: 'src/assets/images/book.svg',
-  },
-  {
-    id: 3,
-    name: 'coffee',
-    image: 'src/assets/images/coffee.svg',
-  },
-  {
-    id: 4,
-    name: 'mountain',
-    image: 'src/assets/images/mountain.svg',
-  },
-  {
-    id: 5,
-    name: 'car',
-    image: 'src/assets/images/car.svg',
-  },
-  {
-    id: 6,
-    name: 'photo',
-    image: 'src/assets/images/photo.svg',
-  },
-  {
-    id: 7,
-    name: 'bread',
-    image: 'src/assets/images/bread.svg',
-  },
-  {
-    id: 8,
-    name: 'cake',
-    image: 'src/assets/images/cake.svg',
-  },
-  {
-    id: 9,
-    name: 'pasta',
-    image: 'src/assets/images/pasta.svg',
-  },
-];
-
-function SearchNavbar() {
+function MapNavbar({ logoImg, color }) {
   const [activeNav, setActiveNav] = useState(1);
+  const navItems = [
+    {
+      id: 1,
+      name: 'home',
+      image: <Home fill={color} />,
+    },
+    {
+      id: 2,
+      name: 'book',
+      image: <Book fill={color} />,
+    },
+    {
+      id: 3,
+      name: 'coffee',
+      image: <Coffee fill={color} />,
+    },
+    {
+      id: 4,
+      name: 'mountain',
+      image: <Mountain fill={color} />,
+    },
+    {
+      id: 5,
+      name: 'car',
+      image: <Car fill={color} />,
+    },
+    {
+      id: 6,
+      name: 'photo',
+      image: <Photo fill={color} />,
+    },
+    {
+      id: 7,
+      name: 'bread',
+      image: <Bread fill={color} />,
+    },
+    {
+      id: 8,
+      name: 'cake',
+      image: <Cake fill={color} />,
+    },
+    {
+      id: 9,
+      name: 'pasta',
+      image: <Pasta fill={color} />,
+    },
+  ];
+
   return (
     <NavbarWrapper>
       <CmapLogo>
-        <img src="src/assets/images/cmapmark.svg" alt="cmapmark" />
-        <span>CMAP</span>
+        <img src={logoImg} alt="cmapmark" />
+        <span style={{ color: color }}>CMAP</span>
       </CmapLogo>
       <NavbarContainer>
         {navItems.map((data) => (
@@ -65,7 +74,7 @@ function SearchNavbar() {
             onClick={() => setActiveNav(data.id)}
             className={activeNav === data.id ? 'active' : ''}
           >
-            <img src={`${data.image}`} alt={data.name} />
+            {data.image}
           </NavItem>
         ))}
       </NavbarContainer>
@@ -76,8 +85,7 @@ function SearchNavbar() {
   );
 }
 
-export default SearchNavbar;
-
+export default MapNavbar;
 const NavbarWrapper = styled.div`
   background-color: #f9fffd;
   width: 120px;
@@ -105,7 +113,6 @@ const CmapLogo = styled.div`
   > span {
     font-size: 20px;
     line-height: 24px;
-    color: #21ae21;
     font-weight: 700;
     margin-top: 13px;
   }
