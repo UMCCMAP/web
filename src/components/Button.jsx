71ce7b00e96f77e251c1436c364c5ed2f58aadd9
lee juyoung <juyung0903@naver.com>
2023-08-01 16:24:21 +0900

    chore: 버튼 컴포넌트 props 항목 추가
    말씀 드렸던 react-quill 및 관련 라이브러리 설치한 것도 편의상 포함했습니다

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,12 @@ const CafeButton = styled.button`
   width: ${(props) => props.width || '70px'}; /* 너비를 props로 받아오고, 없을 경우 70px로 설정 */
   height: ${(props) => props.height || '10px'}; /* 높이를 props로 받아오고, 없을 경우 10px로 설정 */
   border-radius: 16px;
-  background: #ff6868;
+  background: ${(props) => props.background};
+  color: ${(props) => props.color};
+
   cursor: pointer;
-  background: #ff6868;
   padding: 8px;
   border: none;
-  color: white;
   font-family: Pretendard;
   font-size: 15px;
   font-weight: 700;
@@ -18,11 +18,18 @@ const CafeButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  z-index: 999;
 `;
 
-function Button({ name, width, height }) {
+function Button({ name, width, height, background, color, clickHandler }) {
   return (
-    <CafeButton width={width} height={height}>
+    <CafeButton
+      width={width}
+      height={height}
+      onClick={clickHandler}
+      background={background}
+      color={color}
+    >
       {name}
     </CafeButton>
   );
