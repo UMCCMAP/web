d4dfa9daf3be4811ddadbdfc6004af761c442240
minjilee <budlalswl1129@gmail.com>
2023-08-18 17:47:13 +0900

    feat: review delete api

--- src/components/CafeMap/MapReview/ReadReview.jsx
+++ src/components/CafeMap/MapReview/ReadReview.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { format } from 'date-fns';
+import baseAxios from '../../../apis/baseAxios';
 import * as R from './styles/ReadReview.style';
 import { ReactComponent as ReviewStar } from '../../../assets/images/reviewstar.svg';
 import { ReactComponent as Next } from '../../../assets/images/next.svg';
@@ -24,18 +25,48 @@ const settings = {
   ),
 };
 
-function ReadReview({ data, color, closeReview, getReviewIndex }) {
-  const date = data.createdAt ? format(new Date(data.createdAt), 'yyyy.MM.dd') : null;
+function ReadReview({ reviewData, color, closeReview, getReviewIndex }) {
+  const token = localStorage.getItem('accessToken');
+  const userIdx = Number(localStorage.getItem('userIdx'));
+
+  const date = reviewData.data.createdAt
+    ? format(new Date(reviewData.data.createdAt), 'yyyy.MM.dd')
+    : null;
+
+  const clickDeleteBtn = () => {
+    if (window.confirm('해당 리뷰를 삭제하시겠습니까?')) {
+      deleteReview();
+    } else {
+      return;
+    }
+  };
+  const deleteReview = async () => {
+    await baseAxios
+      .delete(`map/place/cafe-reviews/${reviewData.reviewIdx}`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then(function (response) {
+        if (response.status === 204) {
+          alert('삭제되었습니다.');
+          location.reload();
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <R.ReadReviewContainer>
       <R.Title color={color}>
-        <p>{data?.title}</p>
+        <p>{reviewData.data?.title}</p>
         <img src="src/assets/images/close.svg" alt="close" onClick={() => closeReview(0)} />
       </R.Title>
       <hr />
       <R.SubContentWrap>
-        <R.SubContent>{data?.keyword}</R.SubContent>
+        <R.SubContent>{reviewData.data?.keyword}</R.SubContent>
         <R.Scope>
           {[1, 2, 3, 4, 5].map((id) => (
             <ReviewStar
@@ -44,7 +75,7 @@ function ReadReview({ data, color, closeReview, getReviewIndex }) {
               id={id}
               width="16px"
               height="16px"
-              fill={id <= data?.score ? color : '#F1F1F1'}
+              fill={id <= reviewData.data?.score ? color : '#F1F1F1'}
             />
           ))}
         </R.Scope>
@@ -52,31 +83,33 @@ function ReadReview({ data, color, closeReview, getReviewIndex }) {
       <R.UserInfoWrap>
         <R.Date>{date}</R.Date>
         <R.UserInfo>
-          <p>{data?.userInfo?.userNickname}</p>
-          <img src={data?.userInfo?.userImg} alt="user" />
+          <p>{reviewData.data?.userInfo?.userNickname}</p>
+          <img src={reviewData.data?.userInfo?.userImg} alt="user" />
         </R.UserInfo>
       </R.UserInfoWrap>
       <R.ImgWrap>
         <R.StyledSlider {...settings}>
-          {data?.imageUrls?.map((img, index) => (
+          {reviewData.data?.imageUrls?.map((img, index) => (
             <div key={index}>
               <R.SliderImg src={img} alt="image" />
             </div>
           ))}
         </R.StyledSlider>
       </R.ImgWrap>
-      <R.Content>{data?.content}</R.Content>
-      <R.UDWrap>
-        <div
-          onClick={() => {
-            getReviewIndex(2);
-          }}
-        >
-          수정
-        </div>
-        <hr />
-        <div>삭제</div>
-      </R.UDWrap>
+      <R.Content>{reviewData.data?.content}</R.Content>
+      {userIdx === reviewData.data.userInfo?.userIdx ? (
+        <R.UDWrap>
+          <div
+            onClick={() => {
+              getReviewIndex(2);
+            }}
+          >
+            수정
+          </div>
+          <hr />
+          <div onClick={() => clickDeleteBtn()}>삭제</div>
+        </R.UDWrap>
+      ) : undefined}
     </R.ReadReviewContainer>
   );
 }
