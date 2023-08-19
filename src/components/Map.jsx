69ec0444ff86e37e2c1574ff2cb41595e7d64666
minjilee <budlalswl1129@gmail.com>
2023-08-20 03:41:50 +0900

    feat: recommend api

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,59 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { Container as MapDiv, NaverMap, Marker, useNavermaps } from 'react-naver-maps';
 
-const mapItems = [
-  {
-    id: 1,
-    station: '서울시청',
-    latitude: 37.566535,
-    longitude: 126.9779692,
-  },
-  {
-    id: 2,
-    station: '우리집',
-    latitude: 37.5230658,
-    longitude: 126.736356,
-  },
-  {
-    id: 3,
-    station: '방배1',
-    latitude: 37.4859,
-    longitude: 126.997865,
-  },
-  {
-    id: 4,
-    station: '방배2',
-    latitude: 37.48528,
-    longitude: 126.997227,
-  },
-  {
-    id: 5,
-    station: '방배3',
-    latitude: 37.484234,
-    longitude: 126.999292,
-  },
-];
-function Map({ markerImg }) {
+function Map({ markerImg, mapItems }) {
   const navermaps = useNavermaps();
   const [location, setLocation] = useState({ latitude: 37.566535, longitude: 126.9779692 });
   const [map, setMap] = useState(null);
   const [clickedMarkerId, setClickedMarkerId] = useState(null);
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+    if (mapItems.length === 0) return;
+    console.log('ss');
+    if (Array.isArray(mapItems)) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          const location = new navermaps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          map?.setCenter(location);
+          map?.setZoom(15);
         });
-        const location = new navermaps.LatLng(position.coords.latitude, position.coords.longitude);
-        map?.setCenter(location);
-        map?.setZoom(15);
-      });
+      } else {
+        window.alert('현재 위치를 알 수 없습니다.');
+      }
     } else {
-      window.alert('현재 위치를 알 수 없습니다.');
+      navigator.geolocation.getCurrentPosition(() => {
+        setLocation({
+          latitude: mapItems.cafeLatitude,
+          longitude: mapItems.cafeLongitude,
+        });
+        const location = new navermaps.LatLng(mapItems.cafeLatitude, mapItems.cafeLongitude);
+        map?.setCenter(location);
+        map?.setZoom(16);
+      });
     }
-  }, [map]);
+  }, [map, mapItems]);
 
   const handleClickMarker = (lat, long, id) => {
     const clickPosition = new navermaps.LatLng(lat, long);
@@ -85,15 +70,29 @@ function Map({ markerImg }) {
         defaultZoom={15}
         ref={setMap}
       >
-        {mapItems.map((data) => (
+        {Array.isArray(mapItems) ? (
+          <>
+            {mapItems.map((data) => (
+              <Marker
+                key={data?.idx}
+                position={new navermaps.LatLng(data?.cafeLatitude, data?.cafeLongitude)}
+                icon={customMarkerIcon(data?.name, markerImg, data?.idx)}
+                onClick={() =>
+                  handleClickMarker(data?.cafeLatitude, data?.cafeLongitude, data?.idx)
+                }
+              />
+            ))}
+          </>
+        ) : (
           <Marker
-            key={data?.id}
-            position={new navermaps.LatLng(data?.latitude, data?.longitude)}
-            animation={1}
-            icon={customMarkerIcon(data.station, markerImg, data.id)}
-            onClick={() => handleClickMarker(data?.latitude, data?.longitude, data?.id)}
+            key={mapItems.idx}
+            position={new navermaps.LatLng(mapItems.cafeLatitude, mapItems.cafeLongitude)}
+            icon={customMarkerIcon(mapItems.name, markerImg, mapItems.idx)}
+            onClick={() =>
+              handleClickMarker(mapItems.cafeLatitude, mapItems.cafeLongitude, mapItems.idx)
+            }
           />
-        ))}
+        )}
       </NaverMap>
     </MapDiv>
   );
